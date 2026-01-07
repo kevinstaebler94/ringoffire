da7c278fcb249d7c10f3dd9a8ac4359c7487e5a9
Kevin <kevinstaebler1994@outlook.de>
2026-01-07 21:41:51 +0100

    card stack shuffle function implemented

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Game } from "../../models/game";
 
 @Component({
   selector: "app-game",
@@ -7,9 +8,19 @@ import { CommonModule } from "@angular/common";
   templateUrl: "./game.component.html",
   styleUrl: "./game.component.scss",
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  game!: Game;
   constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
